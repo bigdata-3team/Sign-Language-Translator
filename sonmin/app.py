b7a3d5b237d31e4b954fef26c5e25c086fcfbdc2
YUSEONGMIN <chelirt13@naver.com>
2020-08-17 13:30:03 +0900

    Add : Tab service

--- sonmin/app.py
+++ sonmin/app.py
@@ -7,5 +7,36 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+
+@app.route('/index.html')
+def hello_world1():
+    return render_template('index.html')
+
+
+@app.route('/generic.html')
+def hello_world2():
+    return render_template('generic.html')
+
+
+@app.route('/elements.html')
+def hello_world3():
+    return render_template('elements.html')
+
+
+@app.route('/papago.html')
+def hello_world4():
+    return render_template('papago.html')
+
+
+@app.route('/gallery.html')
+def hello_world5():
+    return render_template('gallery.html')
+
+
+@app.route('/webcam.html')
+def hello_world6():
+    return render_template('webcam.html')
+
+
 if __name__ == '__main__':
     app.run()
