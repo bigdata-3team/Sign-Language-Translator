eaf5d25a779d9d9fdd986c9ba0a7f014a1dac7c7
hhseok960@gmail.com <hhseok960@gmail.com>
2020-08-17 17:41:09 +0900

    Add dataset prepare code

---
+++ Model_Build/Dataset_Prepare.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+from PIL import Image
+
+# dataset prepare
+target_folder = "C://Users//shdhk//Desktop//team3_selected"
+folder_list = os.listdir(target_folder)
+
+dataset_annotation = pd.read_excel("수어_데이터셋_어노테이션.xlsx")
+dataset_annotation["folder_name"] = dataset_annotation["파일명"].str[:-4]
+
+input_data = []
+output_data = []
+# for i in range(len(folder_list)):
+for i in range(10):
+    image_path = target_folder + "//{0}".format(folder_list[i])
+    image_list = os.listdir(image_path)
+    print(image_list)
+    """
+    나중에 굳이 추가를 할 필요가 있을까 싶은... sorting을 해야할까?
+    """
+    img_seq = []
+    for j in range(len(image_list)):
+        image = Image.open(image_path + "//{0}".format(image_list[j]))
+        image = np.asarray(image)
+        img_seq.append(image.tolist())
+    input_data.append(img_seq)
+    label = dataset_annotation[dataset_annotation["folder_name"] == folder_list[i]].loc[:, "한국어"].values[0]
+    if type(label) == int:
+        label = str(label)
+    output_data.append(label)
+
+
