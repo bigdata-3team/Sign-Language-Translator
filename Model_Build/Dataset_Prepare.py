994bbd71d790585fe86403a93eafe30251d1905b
hhseok960@gmail.com <hhseok960@gmail.com>
2020-08-17 18:09:14 +0900

    Edit dataset prepare code

--- Model_Build/Dataset_Prepare.py
+++ Model_Build/Dataset_Prepare.py
@@ -13,13 +13,12 @@ dataset_annotation["folder_name"] = dataset_annotation["파일명"].str[:-4]
 
 input_data = []
 output_data = []
-# for i in range(len(folder_list)):
-for i in range(3):
+for i in range(len(folder_list)):
     image_path = target_folder + "//{0}".format(folder_list[i])
     image_list = os.listdir(image_path)
     print(image_list)
     """
-    나중에 굳이 추가를 할 필요가 있을까 싶은... sorting을 해야할까?
+    나중에 굳이 추가를 할 필요가 있을까 싶은... sorting
     """
     img_seq = []
     for j in range(len(image_list)):
@@ -28,7 +27,7 @@ for i in range(3):
         image = image.reshape(-1, *image.shape)
         img_seq.append(image)
     img_seq = np.concatenate(img_seq)
-    img_seq = img_seq.reshape(-1, *img_seq.shape)
+    # img_seq = img_seq.reshape(-1, *img_seq.shape)
     input_data.append(img_seq)
     label = dataset_annotation[dataset_annotation["folder_name"] == folder_list[i]].loc[:, "한국어"].values[0]
     if type(label) == int:
